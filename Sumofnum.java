5704147a1f2085a31c78b53d4ce596def4b08202
Vigneshwaran V <43519088+Vigneshwaran07@users.noreply.github.com>
2019-07-16 11:04:13 +0530

    Update Sumofnum.java

--- Sumofnum.java
+++ Sumofnum.java
@@ -1,27 +1,36 @@
 import java.util.*;
-class Sumofnum
+public class B
 {
-	static int sod(int n)
-	{
-		int tot = 0, rem = 0;
-		while(n!=0)
-		{
-			rem = n%10;
-			tot += rem;
-			n = n/10;
-		}
-		if(tot>9)
-		{
-			tot = sod(tot);
-		}
-		return tot;
-		
-	}
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int result = sod(n);
-		System.out.println(result);
-	}
+    static int f=0;
+    static int sod(int n)
+    {
+        if(n<0)
+        {
+            n = -1*n;
+            f=1;
+        }
+        int tot = 0, rem = 0;
+        while(n!=0)
+        {
+            rem = n%10;
+            tot += rem;
+            n = n/10;
+        }
+        if(tot>9)
+        {
+            tot = sod(tot);
+        }
+        if(f==1)
+        {
+            tot = tot*-1;
+        }
+        return tot;
+    }
+    public static void main(String args[])
+    {
+        sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = sod(n);
+        System.out.println(result);
+    }
 }
