88892d3c025f18779063fe0177da8d7b30807966
Vigneshwaran V <43519088+Vigneshwaran07@users.noreply.github.com>
2019-07-22 11:11:05 +0530

    Create Encodearray.java

---
+++ Encodearray.java
@@ -0,0 +1,25 @@
+package hydra;
+import java.util.*;
+public class Encodedarray
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String[] inp1 = sc.nextLine().split(",");
+		int[] arr = new int[inp1.length];
+		int i = 0;
+		for(String k : inp1)
+		{
+			arr[i++] = Integer.parseInt(k);
+		}
+		for(i=0;i<inp1.length-1;i++)
+		{
+			arr[i] = arr[i] + arr[i+1];
+		}
+		String res = Arrays.toString(arr);
+		System.out.println(res);
+		sc.close();
+	}
+
+}
