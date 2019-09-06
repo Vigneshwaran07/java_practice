ae492f1fde639d5a39c3fda3c4a6f438aecb86f8
Vigneshwaran V <43519088+Vigneshwaran07@users.noreply.github.com>
2019-09-06 12:17:53 +0530

    Update and rename sequence_trial.java to trash.java

---
+++ trash.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class HelloCodiva {
+  
+  public static void main(String[] args)
+  {
+    int x =3, y =3;
+    String pos = "2-2-E", move = "R M L M L M";
+    
+    String movement[] = move.split(" ");
+    
+    int robo_x = Character.getNumericValue(pos.charAt(0)), robo_y = Character.getNumericValue(pos.charAt(2));
+    char direction = pos.charAt(4);
+    for(String i:movement)
+    {
+      
+    }
+    
+  }
+  
+}
+
