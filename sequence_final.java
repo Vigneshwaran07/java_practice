c16b74d93dc39b2e07c6c8fecc7bce27fc521152
Vigneshwaran V <43519088+Vigneshwaran07@users.noreply.github.com>
2019-09-06 09:46:32 +0530

    Create sequence_final.java

---
+++ sequence_final.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Sequence 
+{
+    public static void main(String args[])
+    {
+        int arr[] = {3,2,3,4,5,0};
+        int start = 0, end = 0, i, j, max_size = 0, final_start = 0, final_end = 0, temp_count = 1;
+        for(i=0;i<arr.length-1;i++)
+        {
+            start = i;
+            for(j=i;j<arr.length-1;j++)
+            {
+                if(arr[j]<arr[j+1])
+                {
+                    temp_count += 1;
+                    end = j+1; // end index counts till arr[j+1] 
+                }
+                else
+                {
+                  break; //terminate if it is decreasing series
+                }
+                if(j == arr.length-2 )
+                {
+                    end = j+1;  //for border condition
+                    break;
+                } 
+            }
+            
+            if(max_size < temp_count && temp_count != 1)
+            {
+                max_size = temp_count;
+                final_start = start;
+                final_end = end;
+                temp_count = 1;
+            }
+            if(j == arr.length-2)
+            {
+              break; // break if it is already reach end of an array
+            }
+            
+            
+        }
+        System.out.println(final_start);
+        System.out.println(final_end);
+    }
+}
